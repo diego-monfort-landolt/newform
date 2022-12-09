87127e950c3ae5bf9c98cc5feab3f7d8fe8259e7
Diego19890102 <diegomonfort1989@gmail.com>
2022-12-10 00:09:19 +0100

    animation
    js and css animation h2 and all span

--- main.js
+++ main.js
@@ -6,12 +6,43 @@ var user, password
     user = document.getElementById("usuario").value;
     password = document.getElementById("contrasena").value;
 
+
+
+
     if(user == "Diego" && password == "1234"){
         alert("Login Erfolgreich!");
-       
-    
-
     } else {
         alert("¡Falsche Daten!")
     }
 }
+const text = document.querySelector('.form-control');
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+
+for(let i=0; i < splitText.length; i++){
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+
+}
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char === splitText.length){
+        complete();
+        return;
+
+    }
+
+}
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
+
+
+
